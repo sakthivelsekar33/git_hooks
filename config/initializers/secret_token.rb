fd752f8b345e5853e35e05cf14a1a859e5bc1871
Sakthivel S <sakthivelsekar33@gmail.com>
2014-03-14 18:40:23 +0530

    Commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitHook::Application.config.secret_token = '8d8d230f87d766fbc16f3e435ef526e94497b451430257f216247c35f7e79434c849ff05c77f016787633e494d0c57ff782c9f05639b1402b9d1add05fef67e7'
