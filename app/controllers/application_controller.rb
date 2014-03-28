dd2d5575d3ff90fa321a9c4ed569a3a4a46f9698
Sakthivel S <sakthivelsekar33@gmail.com>
2014-03-28 11:36:58 +0530

    Commit Message 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
+# This is some comment
 class ApplicationController < ActionController::Base
   protect_from_forgery
   puts 'commit 1'
   puts 'commit 2'
-  puts "commit 3"
+  puts 'commit 3'
+  puts 'commit 4'
 end
